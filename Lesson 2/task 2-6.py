a025328cd25ce7479cf451b5f51ddb0db28e8c28
noparapon <dimov.av@gmail.com>
2021-08-03 21:23:08 +0800

    update task 2-6

--- Lesson 2/task 2-6.py
+++ Lesson 2/task 2-6.py
@@ -1,12 +1,13 @@
 my_tuples = []
 i = 1
+keys = ["название", "цена", "количество", "eд"]
 while True:
     name = input("Введите название товара: ")
     price = int(input("Введите цену: "))
     number = int(input("Введите количество: "))
     unit = input("Введите единицу измерения: ")
-    my_tuples.append((i, {"название": name, "цена": price, "количество": number, "eд": unit}))
-    answer = input("[добавлено]\n\nДобавить еще? [+/-]")
+    my_tuples.append((i, {keys[0]: name, keys[1]: price, keys[2]: number, keys[3]: unit}))
+    answer = input("[Ok]\n\nДобавить еще? [+/-]")
     if answer == '-':
         break
     i += 1
@@ -15,9 +16,9 @@ my_dict = {}
 names, prices, nums, units = [], [], [], []
 units = set()
 for one_tuple in my_tuples:
-    names.append(one_tuple[1]["название"])
-    prices.append(one_tuple[1]["цена"])
-    nums.append(one_tuple[1]["количество"])
-    units.add(one_tuple[1]["eд"])
-my_dict.update({"название": names, "цена": prices, "количество": nums, "eд": list(units)})
+    names.append(one_tuple[1][keys[0]])
+    prices.append(one_tuple[1][keys[1]])
+    nums.append(one_tuple[1][keys[2]])
+    units.add(one_tuple[1][keys[3]])
+my_dict.update({keys[0]: names, keys[1]: prices, keys[2]: nums, keys[3]: list(units)})
 print(my_dict)
